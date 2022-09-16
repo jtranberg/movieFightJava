df25929bffdcf5968f7ec53c98b62a71f79f7ff7
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-09-15 21:28:38 -0700

    refactored to add debounce function and delay for api fetch

--- index.js
+++ index.js
@@ -9,15 +9,20 @@ const fetchData = async (searchTerm) => {
 };
 
 const input = document.querySelector('input');
-let timeoutId;
-const onInput = event => {
-   if (timeoutId) {                    //this timeout waits for last key press before fetching api data keeping calls low 
+const debounce = (func, delay = 1000) => {        //default delay
+   let timeoutId;
+   return (...args) => {
+     if (timeoutId) {
       clearTimeout(timeoutId);
-   }
-   timeoutId = setTimeout(() => {
-      fetchData(event.target.value);
-   }, 500)                               //time to wait after last keypress
-   
- };  
-input.addEventListener('input', onInput);
+     }
+     timeoutId = setTimeout(() => {
+      func.apply(null, args);
+     }, delay);
+   };
+};
+
+const onInput = event => {
+   fetchData(event.target.value);
+};  
+input.addEventListener('input',debounce(onInput, 500));  //hard code delay or remove for user input
 
